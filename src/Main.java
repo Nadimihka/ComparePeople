14764fd1dbba4219cc8f46230b68636ea0cfadb5
Nadezhda Krutova <nadiya-yand@yandex.ru>
2022-09-20 17:29:11 +0300

    ComparatorWithLambdaLogic

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,10 +11,30 @@ public class Main {
         people.add(new Person("Сергей", "Серегин-Мценский Запашный-Веденеев", 58));
         people.add(new Person("Поль", "Мария Ремарк", 17));
 
-        Collections.sort(people, new SurnameWordsComparator(3));
-        System.out.println("Персоны в порядке убывания знатности:");
-        for (int i = 0; i < people.size(); i++) {
-            System.out.println(people.get(i).toString());
+        // Задача 1
+        Comparator<Person> comparator;
+        comparator = (a, b) -> {
+
+            Predicate<Integer> isMore = x -> x > 3;
+            int l1 = a.getSurname().split("[ \\-]").length;
+            if (isMore.test(l1)) {
+                l1 = 1;
+            }
+            int l2 = b.getSurname().split("[ \\-]").length;
+            if (isMore.test(l2)) {
+                l2 = 1;
+            }
+
+            if (l1 == l2) {
+                return a.getAge() > b.getAge() ? -1 : 1;
+            } else {
+                return l1 > l2 ? -1 : 1;
+            }
         }
+        ;
+        Collections.sort(people, comparator);
+        System.out.println("Персоны в порядке убывания знатности:");
+        people.forEach(System.out::println);
+
     }
 }
