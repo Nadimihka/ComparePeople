cb4ccc99db7fe2254bead4fe4c3cf2ccfdf2340a
Nadezhda Krutova <nadiya-yand@yandex.ru>
2022-09-20 18:48:56 +0300

    EraseYoungerThen18

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.function.Predicate;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         List<Person> people = new ArrayList<>();
+        people.add(new Person("Антон", "Антонюк", 16));
         people.add(new Person("Иван", "Ипполитов-Иванов", 57));
         people.add(new Person("Петр", "Петров-Водкин Остроумов", 24));
         people.add(new Person("Александр", "Аллександров", 47));
         people.add(new Person("Сергей", "Серегин-Мценский Запашный-Веденеев", 58));
         people.add(new Person("Поль", "Мария Ремарк", 17));
 
-        Collections.sort(people, new SurnameWordsComparator(3));
-        System.out.println("Персоны в порядке убывания знатности:");
-        for (int i = 0; i < people.size(); i++) {
-            System.out.println(people.get(i).toString());
-        }
+        /*Predicate<Person> age = x -> x.getAge() < 18;
+        people.removeIf(age);*/
+        people.removeIf(x -> x.getAge() < 18);
+        System.out.println("Персоны за исключением тех, кто младше 18 лет:");
+        people.forEach(System.out::println);
     }
 }
